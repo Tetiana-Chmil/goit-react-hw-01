004d67fd13fe829bac7ebe0f5c3bba1cd27aa120
Tetiana Chmil <tatiana.l.chmil@gmail.com>
2025-04-04 21:06:51 +0300

    Add transaction history

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,8 @@ import Profile from './Profile/Profile';
 import userData from "../userData.json";
 import FriendList from './FriendList/FriendList';
 import friends from '../friends.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory'
+import transactions from '../transactions.json';
 
 function App() {
   return (
@@ -15,7 +17,9 @@ function App() {
         stats={userData.stats}
       />
 
-      <FriendList friends={friends}/>
+      <FriendList friends={friends} />
+      
+      <TransactionHistory items={transactions} />
     </>
   )
 }
